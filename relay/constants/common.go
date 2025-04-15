a963b0ad1bd21be24310ee8f7196d6e556351bb4
guvanchojamov <guvanchhojamov@gmail.com>
2025-04-15 11:19:46 +0500

    [relay] enchance new Xpos and created constants package

---
+++ relay/constants/common.go
@@ -0,0 +1,17 @@
+package constants
+
+const (
+	DateTimeCustomLayout = "2006/01/02 15:04:05"
+)
+
+// protocols
+const (
+	HTTP = "http"
+	TCP  = "tcp"
+)
+
+// server names
+const (
+	EventServer = "event_server"
+	HTTPGateway = "http_gateway"
+)
